5f36c6a9694665fc5e2537f38a9df335c1403366
ignaciocarrascocl <icarrasco@caput.cl>
2024-09-25 15:58:29 -0300

    Added bootstrap icons and fixed the header.

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<!-- I'm the all mighty default.vue in the layouts. I just show the Header component, Main content (page in this case) and Footer component. In that order. -->
+<template>
+    <Header></Header>
+    <div>
+      <slot />
+    </div>
+    <Footer></Footer>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="text-center">Im the index page. How aboußt that?</h1>
+        <i class="bi bi-alarm"></i>
+    </div>
+</template>
