b963a7c5a331ae59ea0c3b6283e341dd779ddea2
ignaciocarrascocl <icarrasco@caput.cl>
2024-09-25 21:31:27 -0300

    Damn vue local.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="fullpage">
     <!-- Section 1 with background cover and bottom image -->
-    <div class="section section-1 d-flex align-items-end">
+    <div class="section section-1 d-flex align-items-end flex-column">
       <Section1/>
     </div>
 
